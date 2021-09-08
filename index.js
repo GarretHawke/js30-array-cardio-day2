8d6f0a8465fe5faf8eb46d365f1f744609d3317b
GarretHawke <alexandra17au@gmail.com>
2021-09-08 13:54:21 +0300

    feat: add task2 solution

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ console.log(someOlder);
 
 // Array.prototype.every() // is everyone 19 or older?
 
+const everyOlder = people.every(item => new Date().getFullYear() - item.year >= 19);
+console.log(everyOlder);
+
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
